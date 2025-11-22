b46b4a0f6217faae56ff40cbcacc5991bf801583
Maruf Muchlisin <marufmuchlisin22eo10013@students.unugha.id>
2025-11-23 02:40:37 +0700

    Fix formatting and comments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,15 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
+      },
     },
 
-    // ⚠ WAJIB untuk GitHub Pages
+    // ⚠ wajib untuk GitHub Pages
     base: "/UNCA-Apps/",
   };
 });
